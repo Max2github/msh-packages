db10bc272a9e0701cd5ec7be62dd7e711f2ae221
Max2github <Your@email>
2022-11-25 09:04:27 +0100

    up to date with mshgit
    use
    - get_msh_wert
    - MSH_MUTEX : MSH_PRINTMUTEX
    - goto & row change / get msh->info.line
      human lines -> first line == 1, unlike before: first line == 0
    - msh_info

--- std-essential/src/main/essential.c
+++ std-essential/src/main/essential.c
@@ -1,64 +1,76 @@
-/* #include "../../../dependencies/std.h"
-#include "../../../include/alg.h"
-#include "../../../include/msh.h"
-#include "../../../dependencies/words.h" */
-
 #include "../../../../mshgit/dependencies/std.h"
+
 #include "../../../../mshgit/include/alg.h"
 #include "../../../../mshgit/include/msh.h"
-#include "../../../../mshgit/dependencies/words.h"
+
+#include "../../../../mshgit/dependencies/extern.h"
 
 void msh_command_main_display(msh_info * msh) {
+    char * wert = get_msh_Wert(msh);
+
     if (find(msh_Wert, "&/ref//")) {
-        MSH_PRINTF(">> ");
+        MSH_MUTEX_LOCK(MSH_PRINT_MUTEX);
+        MSH_PRINTF_NO_FLUSH(msh, ">> ");
         char ** withRef;
-        int anzahlSpl = split(msh_Wert, "&/ref//", &withRef);
+        int anzahlSpl = split(wert, "&/ref//", &withRef);
         for (int i = 0; i <= anzahlSpl; i++) {
             if (i % 2 /* == 1 */) { // each second is a reference - begins with 0
                 superstring tempS = s_init(withRef[i]);
-                MSH_PRINTF("%s", (char *) msh_ref_get(msh, (index64) tempS));
+                MSH_PRINTF_NO_FLUSH(msh, "%s", (char *) msh_ref_get(msh, (index64) tempS));
                 s_free(tempS);
                 continue;
             }
-            MSH_PRINTF("%s", withRef[i]);
+            MSH_PRINTF_NO_FLUSH(msh, "%s", withRef[i]);
         }
-        MSH_PRINTF("\n");
+        MSH_PRINTF_NO_FLUSH(msh, "\n");
+        msh_flush(msh);
+        MSH_MUTEX_UNLOCK(MSH_PRINT_MUTEX);
         freeWordArr(withRef, anzahlSpl);
         return;
     }
 
-    if (find(msh_Wert, "&/arr//") != 0) {
-        replaceS(msh_Wert, "&/arr//", "\n     ");
-        MSH_PRINTF(">> [\n     %s\n   ]\n", msh_Wert);
-    } else if (find(msh_Wert, "&/obj//") != 0) {
-        // printf("%s\n", VAR_SPEICHER[0]);
-        replaceS(msh_Wert, "&/obj//", "\n     ");
-        MSH_PRINTF(">> {\n     %s\n   }\n", msh_Wert);
+    if (find(wert, "&/arr//") != 0) {
+        replaceS(wert, "&/arr//", "\n     ");
+        MSH_MUTEX_LOCK(MSH_PRINT_MUTEX);
+        MSH_PRINTF(msh, ">> [\n     %s\n   ]\n", wert);
+        MSH_MUTEX_UNLOCK(MSH_PRINT_MUTEX);
+    } else if (find(wert, "&/obj//") != 0) {
+        replaceS(wert, "&/obj//", "\n     ");
+        MSH_MUTEX_LOCK(MSH_PRINT_MUTEX);
+        MSH_PRINTF(msh, ">> {\n     %s\n   }\n", wert);
+        MSH_MUTEX_UNLOCK(MSH_PRINT_MUTEX);
     } else {
-        MSH_PRINTF(">> %s\n", msh_Wert);
+        MSH_MUTEX_LOCK(MSH_PRINT_MUTEX);
+        MSH_PRINTF(msh, ">> %s\n", wert);
+        MSH_MUTEX_UNLOCK(MSH_PRINT_MUTEX);
     };
 };
 void msh_command_main_print(msh_info * msh) {
-    if (find(msh_Wert, "&/arr//") != 0) {
-        MSH_PRINTF("!! ERROR at line %d : Cannot \"print()\" variable of type \"Array\"!", msh_Script_it + 1);
-    } else if (find(msh_Wert, "&/obj//") != 0) {
-        MSH_PRINTF("!! ERROR at line %d : Cannot \"print()\" variable of type \"object\"!", msh_Script_it + 1);
+    char * wert = get_msh_Wert(msh);
+
+    if (find(wert, "&/arr//") != 0) {
+        msh_error(msh, "Cannot \"print()\" variable of type \"array\"!");
+    } else if (find(wert, "&/obj//") != 0) {
+        msh_error(msh, "Cannot \"print()\" variable of type \"object\"!");
     } else {
-        replaceS(msh_Wert, "\\n", "\n");
-        MSH_PRINTF("%s", msh_Wert);
+        replaceS(wert, "\\n", "\n");
+        MSH_MUTEX_LOCK(MSH_PRINT_MUTEX);
+        MSH_PRINTF(msh, "%s", wert);
+        MSH_MUTEX_UNLOCK(MSH_PRINT_MUTEX);
     };
 };
 void msh_command_main_stop(msh_info * msh) {
-    msh_STOP = 1;
+    msh->event.stop = true;
 };
 void msh_command_main_del(msh_info * msh) {
+    char * wert = get_msh_Wert(msh);
+
     char var[VAR_MAXCHAR];
-    int index = msh_get_Var(msh_Wert, var);
+    int index = msh_get_Var(wert, var);
     if (index == -1) {
         char text[] = "Variable &/var// does not exist!";
-        replaceS(text, "&/var//", msh_Wert);
-        // word_copy(msh_Wert, text);
-        set_msh_Wert(text);
+        replaceS(text, "&/var//", wert);
+        set_msh_Wert(msh, text);
     } else {
         for (int i = index; i < VAR_WORTZAELER-1; i++) {
             word_copy(VAR_NAMES[i], VAR_NAMES[i+1]);
@@ -69,10 +81,9 @@ void msh_command_main_del(msh_info * msh) {
     };
 };
 void msh_command_main_call(msh_info * msh) {
-    msh_func_call(msh, msh_Wert);
+    msh_func_call(msh, get_msh_Wert(msh));
 };
 void msh_command_main_goto(msh_info * msh) {
-    int ln = MSH_STRING_TO_INT(msh_Wert);
-    msh_Script_it = ln - 1;
+    int ln = MSH_STRING_TO_INT(get_msh_Wert(msh));
     msh->info.line = ln;
 };
--- std-essential/src/sub/essential.c
+++ std-essential/src/sub/essential.c
@@ -1,32 +1,30 @@
-/* #include "../../../include/alg.h"
-#include "../../../dependencies/words.h"
-#include "../../../dependencies/std.h"
-#include "../../../dependencies/mathe.h"
-
-#include "../../cparts/command_def.h" */
-
 #include "../../../../mshgit/dependencies/std.h"
-#include "../../../../mshgit/include/alg.h"
-#include "../../../../mshgit/dependencies/words.h"
-#include "../../../../mshgit/dependencies/mathe.h"
 
+#include "../../../../mshgit/include/alg.h"
+#include "../../../../mshgit/dependencies/extern.h"
+
+// this will include all other commands in this package
 #include "../../../../mshgit/src/cparts/command_def.h"
 
 void msh_command_sub_input(msh_info * msh) {
+    char * wert = get_msh_Wert(msh);
+
     msh_command_main_print(msh); // will print msh_Wert
     // scanf(" %s", msh_Wert);
     // fgets(msh_Wert, VAR_MAXCHAR-1, stdin); // this could cause problems
     // char * temp; size_t len = 0;
     // getline(&temp, &len, stdin); word_copy(msh_Wert, temp); free(temp);
-    char * wertP = msh_Wert;
+    char * wertP = wert;
     char now = 'n';
     while(now != '\n') { *wertP = getchar(); now = *wertP; wertP++; }
     *wertP = '\0';
-    replaceS(msh_Wert, "\n", "");
-    replaceS(msh_Wert, "=", "&/equals//");
+    replaceS(wert, "\n", "");
+    replaceS(wert, "=", "&/equals//");
 };
 void msh_command_sub_math(msh_info * msh) {
-    double erg = rechnen(msh_Wert);
+    char * wert = get_msh_Wert(msh);
+
+    double erg = rechnen(wert);
     // Kommastellen mit 0 am Ende entfernen
     char newWert[VAR_MAXCHAR];
     char repl[VAR_MAXCHAR];
@@ -46,9 +44,9 @@ void msh_command_sub_math(msh_info * msh) {
     if (last == 0) {
         newWert[0] = '\0';
     };
-    char * nWertP = msh_Wert;
+    char * nWertP = wert;
     if (neg) {
-        set_msh_Wert("-");
+        set_msh_Wert(msh, "-");
         nWertP++;
     }
     word_copy(nWertP, repl);
