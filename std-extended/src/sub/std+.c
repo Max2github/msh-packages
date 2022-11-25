e574047664421899578b76aecfa639a0b18a3007
Max2github <Your@email>
2022-11-25 09:08:24 +0100

    std-extended: use msh_info and realted funtions

--- std-extended/src/sub/std+.c
+++ std-extended/src/sub/std+.c
@@ -5,45 +5,50 @@
 
 #include "../../cparts/command_def.h" */
 
-#include "../../../../mshgit/include/alg.h"
-#include "../../../../mshgit/dependencies/words.h"
 #include "../../../../mshgit/dependencies/std.h"
-#include "../../../../mshgit/dependencies/mathe.h"
 
-void msh_command_sub_array() {
+#include "../../../../mshgit/include/alg.h"
+
+#include "../../../../mshgit/dependencies/extern.h"
+
+void msh_command_sub_array(msh_info * msh) {
+    char * wert = get_msh_Wert(msh);
+
     char ** array;
-    int arrTeile = split(msh_Wert, "IN", &array);
+    int arrTeile = split(wert, "IN", &array);
     if (arrTeile > 0) {
         if(word_compare(array[1], "&/null//") == 0 || word_compare(array[1], "") == 0) {
-            set_msh_Wert("&/null//&/arr//&/null//");
+            set_msh_Wert(msh, "&/null//&/arr//&/null//");
             return;
         };
 
-        set_msh_Wert(array[1]);
-        replaceS(msh_Wert, array[0], "&/arr//");
+        set_msh_Wert(msh, array[1]);
+        replaceS(wert, array[0], "&/arr//");
 
-        if (find(msh_Wert, "&/arr//") == 0) {
-            word_add(msh_Wert, "&/arr//&/null//");
-        } else if (word_compare(msh_Wert, "&/arr//") == 0) {
-            set_msh_Wert("&/null//&/arr//&/null//");
+        if (find(wert, "&/arr//") == 0) {
+            word_add(wert, "&/arr//&/null//");
+        } else if (word_compare(wert, "&/arr//") == 0) {
+            set_msh_Wert(msh, "&/null//&/arr//&/null//");
         } else {
-            replaceS(msh_Wert, "&/arr//&/arr//", "&/arr//&/null//&/arr//");
+            replaceS(wert, "&/arr//&/arr//", "&/arr//&/null//&/arr//");
         };
     };
     freeWordArr(array, arrTeile);
 };
-void msh_command_sub_object() {
+void msh_command_sub_object(msh_info * msh) {
+    char * wert = get_msh_Wert(msh);
+
     char ** object;
-    int objTeile = split(msh_Wert, "IN", &object);
+    int objTeile = split(wert, "IN", &object);
     if (objTeile > 0) {
-        set_msh_Wert(object[1]);
-        replaceS(msh_Wert, object[0], "&/obj//");
+        set_msh_Wert(msh, object[1]);
+        replaceS(wert, object[0], "&/obj//");
     }
     freeWordArr(object, objTeile);
 };
-void msh_command_sub_random() {
+void msh_command_sub_random(msh_info * msh) {
     char ** zahlen;
-    int zahlTeile = split(msh_Wert, "&/arr//", &zahlen);
+    int zahlTeile = split(get_msh_Wert(msh), "&/arr//", &zahlen);
     int MIN = atoi(zahlen[0]);
     int MAX = atoi(zahlen[1]);
     int Komma = atoi(zahlen[2]);
@@ -51,23 +56,25 @@ void msh_command_sub_random() {
     char sr[VAR_MAXCHAR];
     doubleToString(r, atoi(zahlen[2]), sr);
 
-    set_msh_Wert(sr);
+    set_msh_Wert(msh, sr);
     freeWordArr(zahlen, zahlTeile);
 };
-void msh_command_sub_randomin() {
+void msh_command_sub_randomin(msh_info * msh) {
+    const char * wert = get_msh_Wert(msh);
+
     char ** elems;
-    int elemTeile = split(msh_Wert, "&/arr//", &elems);
+    int elemTeile = split(wert, "&/arr//", &elems);
     char arr[] = "0&/arr//%d&/arr//0";
     word_copy(arr, "0&/arr//");
     intToString(elemTeile, arr+8); // not very beautifull
-    set_msh_Wert(arr);
-    msh_command_sub_random();
-    set_msh_Wert(elems[atoi(msh_Wert)]);
+    set_msh_Wert(msh, arr);
+    msh_command_sub_random(msh);
+    set_msh_Wert(msh, elems[atoi(wert)]);
     freeWordArr(elems, elemTeile);
 };
-void msh_command_sub_row() {
-    intToString(msh_Script_it, msh_Wert);
+void msh_command_sub_row(msh_info * msh) {
+    intToString(msh->info.line, get_msh_Wert(msh));
 };
-void msh_command_sub_ascii() {
-    intToString((int) msh_Wert[0], msh_Wert);
+void msh_command_sub_ascii(msh_info * msh) {
+    intToString((int) msh_Wert[0], get_msh_Wert(msh));
 };
